1f3495a37a0c1ca4f85ccb3e358daa55de50ea62
Gatsby Lee <thewayofthinking@gmail.com>
2019-05-09 19:50:45 -0700

    Update z_gcp_storage.py

--- storage/gcp-storage/z_gcp_storage.py
+++ storage/gcp-storage/z_gcp_storage.py
@@ -16,27 +16,45 @@ class GCPStorage(object):
         self._client = storage.Client()
         self.bucket = self._client.bucket(bucket_name)
 
-    def store(self, content, object_key: str,
-              content_type: str,
-              content_encoding: str):
+    def store_str(self, content, object_key: str,
+                  content_type: str,
+                  content_encoding: str):
         blob = self.bucket.blob(object_key)
         blob.content_encoding = content_encoding
         blob.upload_from_string(content,
                                 content_type=content_type)
 
+    def fetch_str(self, object_key):
+        blob = self.bucket.blob(object_key)
+        return blob.download_as_string()
+
 
 def _parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--bucket', required=True)
-    parser.add_argument('--content', required=True)
-    parser.add_argument('--object-key', required=True)
-    parser.add_argument('--content-type', required=True)
-    parser.add_argument('--content-encoding', required=True)
+
+    cmd_parser = parser.add_subparsers(dest='cmd', required=True)
+    store_str_parser = cmd_parser.add_parser('store-str')
+    store_str_parser.add_argument('--bucket', required=True)
+    store_str_parser.add_argument('--content', required=True)
+    store_str_parser.add_argument('--object-key', required=True)
+    store_str_parser.add_argument('--content-type', required=True)
+    store_str_parser.add_argument('--content-encoding', required=True)
+    fetch_str_parser = cmd_parser.add_parser('fetch-str')
+    fetch_str_parser.add_argument('--bucket', required=True)
+    fetch_str_parser.add_argument('--object-key', required=True)
     return parser.parse_args()
 
 
 def cli_main():
     args = _parse_args()
     s = GCPStorage(args.bucket)
-    content = args.content
-    s.store(content, args.object_key, args.content_type, args.content_encoding)
+    if args.cmd == 'store-str':
+        content = args.content
+        s.store_str(content, args.object_key, args.content_type, args.content_encoding)
+    elif args.cmd == 'fetch-str':
+        r = s.fetch_str(args.object_key)
+        print(r)
+
+
+if __name__ == '__main__':
+    cli_main()
