6a60a95f13963fb4e73a491b264dc405d6b01b0a
Gatsby Lee <thewayofthinking@gmail.com>
2022-08-21 23:48:05 -0700

    Update start_wait_query.py

--- aws_cloud/aws_athena/start_wait_query.py
+++ aws_cloud/aws_athena/start_wait_query.py
@@ -6,7 +6,7 @@ def run_query(db_name: str, table_name:str):
     client = boto3.client("athena")
 
     # run query
-    query = f"SELECT count(1) as count FROM {db_name}.{table_name}"
+    query = f"SELECT org_id, count(1) as count FROM {db_name}.{table_name} GROUP BY org_id"
     LOGGER.info("query=%s", query)
     response = client.start_query_execution(
         QueryString=query,
@@ -33,5 +33,15 @@ def run_query(db_name: str, table_name:str):
     response = client.get_query_results(
         QueryExecutionId=query_execution_id, MaxResults=1000
     )
-    
-    return response
+
+    # {'Data': [{'VarCharValue': 'org_id'}, {'VarCharValue': 'count'}]}
+    # {'Data': [{'VarCharValue': '204'}, {'VarCharValue': '12'}]}
+    result = {}
+    for idx, row in enumerate(response["ResultSet"]["Rows"]):
+        if idx != 0:
+            col_1, col_2 = row["Data"]
+            org_id = col_1["VarCharValue"]
+            count = int(col_2["VarCharValue"])
+            result[org_id] = count
+
+    return result
